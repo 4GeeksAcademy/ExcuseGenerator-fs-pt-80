e3956a568ab1515e3504fc1e800847dc0d116fa4
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-10-02 18:13:50 +0000

    starting

--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
